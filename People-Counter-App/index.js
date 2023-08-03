e1c14a1a097a7b389e270b265a56c19ece9846a6
Get15Gonzo <yourgithub@email.com>
2023-08-03 14:16:26 -0400

    Progressing through counter tutorial

---
+++ People-Counter-App/index.js
@@ -0,0 +1,14 @@
+document.getElemntById("count-el").innerText = 5
+// let count = 5
+// count = count + 1
+// console.log(count)
+
+// let bonusPoints = 50
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
