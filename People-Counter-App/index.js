3bbbdc4912bab4afdd787bb96fae68d130517d1e
Get15Gonzo <yourgithub@email.com>
2023-08-03 20:01:38 -0400

    connected the person counter and am making the save function

--- People-Counter-App/index.js
+++ People-Counter-App/index.js
@@ -1,4 +1,4 @@
-document.getElemntById("count-el").innerText = 5
+// document.getElemntById("count-el").innerText = 5
 // let count = 5
 // count = count + 1
 // console.log(count)
@@ -12,3 +12,26 @@ document.getElemntById("count-el").innerText = 5
 
 // bonusPoints = bonusPoints + 45
 // console.log(bonusPoints)
+
+let countEl = document.getElementById("count-el")
+let count = 0
+
+const incrementCount = () => {
+    count = (count + 1)
+    countEl.innerText = count
+}
+
+let saveEl = doucment.getElementById("save-el")
+
+const saveCount = () => {
+        saveEl.innerText = ('countEl')
+}
+
+let names = "John"
+let greeting = "Hi, my name is"
+let myGreeting = names + " " + greeting + "!"
+console.log(myGreeting)
+
+
+
+
