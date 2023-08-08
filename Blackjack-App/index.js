8ec167d6810583c48786698eba450cd7c3e0d15f
Get15Gonzo <yourgithub@email.com>
2023-08-08 17:18:53 -0400

    adding much functionality to the blackjack game. Trying to fix the getRandomCard function. It is currently not returning the desired outcome.

--- Blackjack-App/index.js
+++ Blackjack-App/index.js
@@ -1,22 +1,48 @@
-let firstCard = 10
-let secondCard = 11
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+// let sumEl = document.getElementById("sum-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-const startgame = () => {
+const getRandomCard = () => {
+    return 5;
+}
+
+const startGame = () => {
+    renderGame()
+}
+
+const renderGame = () => {
+    cardsEl.textContent = "Cards: "
+    
+    for(i = 0; i < cards.length; i ++) {
+        cardsEl.textContent += cards[i] + " " 
+    }
+
+    sumEl.textContent = "Sum: " + sum
+    
     if (sum <= 20) {
         message ="Do you want to draw another?"
-        console.log(message)
     } else if (sum === 21) {
         message ="Whoo! Blackjack!"
-        console.log(message)
         hasBlackJack = true
     } else {
         message ="You're out of the game!"
-        console.log(message)
         isAlive = false
     }
+    
+    messageEl.textContent = message
+}
+
+const newCard = () => {
+    let card = getRandomCard()
+    sum += card
+    cards.push (card)
+    renderGame()
 }
