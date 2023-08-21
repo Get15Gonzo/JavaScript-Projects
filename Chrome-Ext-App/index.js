d0107a7ddff9e6bf2fd7e415f66e96113afcdc23
Get15Gonzo <yourgithub@email.com>
2023-08-21 15:39:15 -0400

    Finished extension. Added tab button which communicates with chrome tab api so it gets the tab you are currently on

--- Chrome-Ext-App/index.js
+++ Chrome-Ext-App/index.js
@@ -1,6 +1,7 @@
 let myLeads = []
 const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
@@ -25,6 +26,15 @@ function render(leads) {
     }
 ulEl.innerHTML = listItems
 }
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+
+    })
+})
 
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
